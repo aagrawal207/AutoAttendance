c4c509b0cfefdc647802286cbdf30b30c19a530c
Abhishek Agrawal <Abhishek Agrawal@ABHISHEKAGRAWAL>
2017-02-12 14:14:06 +0530

    Renamed Files and forms

--- AutoAttendance/CameraCapture.Designer.cs
+++ AutoAttendance/CameraCapture.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AutoAttendance
 {
-    partial class Form1
+    partial class CameraCapture
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CameraCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "CameraCapture";
+            this.Text = "CameraCapture";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- AutoAttendance/CameraCapture.cs
+++ AutoAttendance/CameraCapture.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace AutoAttendance
 {
-    public partial class Form1 : Form
+    public partial class CameraCapture : Form
     {
-        public Form1()
+        public CameraCapture()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
