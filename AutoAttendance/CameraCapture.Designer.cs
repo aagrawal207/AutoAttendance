de311a82526aad38a6eece43f94b73997be8558f
Abhishek Agrawal <Abhishek Agrawal@ABHISHEKAGRAWAL>
2017-02-14 01:42:28 +0530

    Camera is added in the application.

--- AutoAttendance/CameraCapture.Designer.cs
+++ AutoAttendance/CameraCapture.Designer.cs
@@ -28,21 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.CamImageBox = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // CamImageBox
+            // 
+            this.CamImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CamImageBox.Location = new System.Drawing.Point(12, 12);
+            this.CamImageBox.Name = "CamImageBox";
+            this.CamImageBox.Size = new System.Drawing.Size(329, 294);
+            this.CamImageBox.TabIndex = 2;
+            this.CamImageBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(265, 313);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // CameraCapture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(441, 352);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.CamImageBox);
             this.Name = "CameraCapture";
-            this.Text = "CameraCapture";
+            this.Text = "Camera Output";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox CamImageBox;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
--- AutoAttendance/CameraCapture.cs
+++ AutoAttendance/CameraCapture.cs
@@ -7,19 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
 
-namespace AutoAttendance
-{
-    public partial class CameraCapture : Form
-    {
-        public CameraCapture()
-        {
+namespace AutoAttendance {
+    public partial class CameraCapture : Form {
+        //declaring global variables
+        private Capture capture;        //takes images from camera as image frames
+        private bool captureInProgress; // checks if capture is executing
+
+        private void ProcessFrame(object sender, EventArgs arg) {
+            Image<Bgr, Byte> ImageFrame = capture.QueryFrame();  //line 1
+            CamImageBox.Image = ImageFrame;        //line 2
+        }
+
+        public CameraCapture() {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) {
 
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            #region if capture is not created, create it now
+            if (capture == null)
+            {
+                try
+                {
+                    capture = new Capture();
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            #endregion
+
+            if (capture != null)
+            {
+                if (captureInProgress)
+                {  //if camera is getting frames then stop the capture and set button Text
+                    // "Start" for resuming capture
+                    btnStart.Text = "Start!"; //
+                    Application.Idle -= ProcessFrame;
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+                    btnStart.Text = "Stop";
+                    Application.Idle += ProcessFrame;
+                }
+                captureInProgress = !captureInProgress;
+            }
+        }
+        private void ReleaseData() {
+            if (capture != null)
+                capture.Dispose();
         }
     }
 }
