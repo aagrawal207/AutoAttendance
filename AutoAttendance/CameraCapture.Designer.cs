c0e8c0598a20a16519e4c2c92fa6e0c7c8e1bcda
Abhishek Agrawal <Abhishek Agrawal@ABHISHEKAGRAWAL>
2017-02-15 16:57:33 +0530

    Face detection from a given image, done.

--- AutoAttendance/CameraCapture.Designer.cs
+++ AutoAttendance/CameraCapture.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.CamImageBox = new Emgu.CV.UI.ImageBox();
             this.btnStart = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,25 +40,36 @@
             this.CamImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.CamImageBox.Location = new System.Drawing.Point(12, 12);
             this.CamImageBox.Name = "CamImageBox";
-            this.CamImageBox.Size = new System.Drawing.Size(329, 294);
+            this.CamImageBox.Size = new System.Drawing.Size(559, 430);
             this.CamImageBox.TabIndex = 2;
             this.CamImageBox.TabStop = false;
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(265, 313);
+            this.btnStart.Location = new System.Drawing.Point(496, 448);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 23);
             this.btnStart.TabIndex = 3;
-            this.btnStart.Text = "Start";
+            this.btnStart.Text = "Webcam";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(415, 448);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 4;
+            this.btnBrowse.Text = "pic1.jpeg";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
             // CameraCapture
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(441, 352);
+            this.ClientSize = new System.Drawing.Size(583, 480);
+            this.Controls.Add(this.btnBrowse);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.CamImageBox);
             this.Name = "CameraCapture";
@@ -72,6 +84,7 @@
 
         private Emgu.CV.UI.ImageBox CamImageBox;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnBrowse;
     }
 }
 
--- AutoAttendance/CameraCapture.cs
+++ AutoAttendance/CameraCapture.cs
@@ -10,13 +10,16 @@ using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.Util;
+using Emgu.CV.CvEnum;
 
 namespace AutoAttendance {
     public partial class CameraCapture : Form {
         //declaring global variables
         private Capture capture;        //takes images from camera as image frames
         private bool captureInProgress; // checks if capture is executing
+
         private HaarCascade haar;
+
         private void ProcessFrame(object sender, EventArgs arg) {
             Image<Bgr, Byte> ImageFrame = capture.QueryFrame();  //line 1
             CamImageBox.Image = ImageFrame;        //line 2
@@ -62,6 +65,24 @@ namespace AutoAttendance {
         private void ReleaseData() {
             if (capture != null)
                 capture.Dispose();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e) {
+            Image InputImg = Image.FromFile(@"C:\Users\Abhishek Agrawal\Pictures\pic1.jpeg");
+            Image<Bgr, byte> ImageFrame = new Image<Bgr, byte>(new Bitmap(InputImg));
+            if (ImageFrame != null)
+            {
+                Image<Gray, byte> grayframe = ImageFrame.Convert<Gray, byte>();
+                var faces = grayframe.DetectHaarCascade(haar, 1.4, 4, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(25, 25))[0];
+                foreach (var face in faces)
+                {
+                    ImageFrame.Draw(face.rect, new Bgr(Color.Green), 3);
+                }
+                CamImageBox.Image = ImageFrame;
+                int TotalFaces = faces.Length;   // you can use TotalFaces from  here on, i'll only display it:
+
+                MessageBox.Show("Total faces detected: " + TotalFaces.ToString());
+            }
         }
     }
 }
